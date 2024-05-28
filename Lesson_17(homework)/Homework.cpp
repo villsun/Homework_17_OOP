1cda9f6106cbcf8005602599ac7087b515e0872a
villsun <lonukkola@gmail.com>
2024-05-28 19:37:58 +0200

    Add fucntion Reading_File

--- Lesson_17(homework)/Homework.cpp
+++ Lesson_17(homework)/Homework.cpp
@@ -1,10 +1,41 @@
-#include <iostream>
+﻿#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <map>
 
 using namespace std;
 
-int main()
+void Reading_File(multimap<string, string>& dictionary)
 {
+	ifstream inputFile("F:\\Visual Studio project object-oriented programming\\Lesson_17(homework)\\Dictionary.txt");
+	if (!inputFile) {
+		cerr << "Error opening file" << endl;
+		return;
+	}
+
+	string line;
+	while (getline(inputFile, line))
+	{
+		istringstream iss(line);
+		string firstWord, secondWord;
+
+		if (iss >> firstWord >> secondWord) {
+			dictionary.insert({ firstWord, secondWord });
+		}
+		else {
+			cerr << "Error parsing line" << endl;
+		}
+	}
+
+	inputFile.close();
+}
 
 
+int main() {
+
+	multimap<string, string> dictionary;
+	Reading_File(dictionary);
+	
 	return 0;
 }
